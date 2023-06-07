74f471c22915e5fd178b755a3b2935466d55b8ca
Logon Aluno <logonrmlocal@fiap.com.br>
2023-06-07 09:29:17 -0300

    desafiosjs

--- revisao/exemplo.js
+++ revisao/exemplo.js
@@ -1,14 +1,11 @@
 const leia = require("prompt-sync")()
 
-
-
 let valor1 = 0
 let valor2 = 0
 
 valor1 = parseInt(leia("Digite o valor 1"))
 valor2 = parseInt(leia("digite o valor 2"))
 let nome = leia("Digite seu nome")
-
 
 console.log(valor1 + valor2)
 console.log(nome)
